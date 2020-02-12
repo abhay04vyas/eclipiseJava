d60f13fe94570f566b5e8a5d390efa6245c480fe
abhay04vyas <45718317+abhay04vyas@users.noreply.github.com>
2020-02-12 15:26:32 +0530

    Update App.java
    Gan Gan Ganat Bote

--- eclipiseJava/src/main/java/com/javaGitHub/eclipiseJava/App.java
+++ eclipiseJava/src/main/java/com/javaGitHub/eclipiseJava/App.java
@@ -8,7 +8,8 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Om Shree Gajanan Maharaj Ki Jai... Gan Gan Ganat Bote... Om Gajanan Shree Gajanan Jay Gajanan Guru Gajanana" );
-        
+        System.out.println( "Om Shree Gajanan Maharaj Ki Jai..." );
+        System.out.println( "Gan Gan Ganat Bote..." );
+        System.out.println( "Om Gajanan Shree Gajanan Jay Gajanan Guru Gajanana..." );
     }
 }
